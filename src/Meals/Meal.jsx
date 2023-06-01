fbc887b28be7ae6baccb1acc6750b25aa882ad9d
anandhu-as <anandhuas010203@gmail.com>
2023-06-01 16:39:29 +0530

    added loading

--- src/Meals/Meal.jsx
+++ src/Meals/Meal.jsx
@@ -1,31 +1,39 @@
 import { React, useState, useEffect } from "react";
 import "../Meals/Meal.css";
+
 function Meal() {
   const [meals, setMeals] = useState([]);
-  const [loading,setLoading]=useState(true);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=c")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.meals);
+        console.log(data.meals[0].strYoutube);
         const mealNames = data.meals.map((meal) => ({
           img: meal.strMealThumb,
           name: meal.strMeal,
         }));
         setMeals(mealNames);
-        setLoading(false)
+        setLoading(false);
       });
   }, []);
-
   return (
-    
-    <div className="container">
-      {meals.map((meal) => (
-        <div className="meal" key={meal.name}>
-          <img src={meal.img} alt={meal.name} />
-          <h3>{meal.name}</h3>
+    <div>
+      {loading ? (
+        <div className="loader">
+          <div className="justify-content-center jimu-primary-loading"></div>
         </div>
-      ))}
+      ) : (
+        <div className="container">
+          {meals.map((meal) => (
+            <div className="meal" key={meal.name}>
+              <img src={meal.img} alt={meal.name} />
+              <h3>{meal.name}</h3>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
